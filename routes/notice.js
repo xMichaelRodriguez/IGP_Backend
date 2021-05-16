8aa3b291bf6d42d999e7a6773260420ae9a4d0a8
xMichaelRodriguez <scottlovos503@gmail.com>
2021-05-16 11:11:28 -0600

    update

--- routes/notice.js
+++ routes/notice.js
@@ -12,8 +12,16 @@ router.use(validarJWT);
 router.post(
   "/newNotice",
   [
-    check("title", "The title is required").notEmpty(),
-    check("body", "The body is required"),
+    //middleware
+    check("title", "Mandatory title and minimum of 6 characters")
+      .notEmpty()
+      .isLength({ min: 6 }),
+    check(
+      "body",
+      "Mandatory body and minimum of 50 characters and maximum of 2000 characters "
+    )
+      .notEmpty()
+      .isLength({ min: 50, max: 2000 }),
     validarCampos,
   ],
   constroller.newNotice
@@ -21,16 +29,21 @@ router.post(
 router.put(
   "/editNotice/:id",
   [
-    check("title", "The title is required").notEmpty(),
-    check("body", "The body is required"),
+    //middleware
+    check("title", "Mandatory title and minimum of 6 characters")
+      .notEmpty()
+      .isLength({ min: 6 }),
+    check(
+      "body",
+      "Mandatory body and minimum of 50 characters and maximum of 2000 characters "
+    )
+      .notEmpty()
+      .isLength({ min: 50, max: 2000 }),
     validarCampos,
   ],
   constroller.editNotice
 );
 
-router.delete(
-  "/deleteNotice/:id",
-  constroller.deleteNotice
-);
+router.delete("/deleteNotice/:id", constroller.deleteNotice);
 
 module.exports = router;
